acd8475d1b4c5051c026824edf12b31988b2313c
obadahazima <obhazima@gmail.com>
2026-06-28 18:21:04 +0300

    Update custom-fetch.ts

--- lib/api-client-react/src/custom-fetch.ts
+++ lib/api-client-react/src/custom-fetch.ts
@@ -15,7 +15,7 @@ const DEFAULT_JSON_ACCEPT = "application/json, application/problem+json";
 // Module-level configuration
 // ---------------------------------------------------------------------------
 
-let _baseUrl: string | null = null;
+let _baseUrl: string | null = "https://workspaceapi-server-production-85e3.up.railway.app";
 let _authTokenGetter: AuthTokenGetter | null = null;
 
 /**
